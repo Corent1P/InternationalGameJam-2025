1baf17bcfb48eb3628372582eb27e2eb5a61e3bf
glaentzlin kevin <kevin1.glaentzlin@epitech.eu>
2025-10-17 03:35:17 +0700

    add new trap sound detector, with mp3 bip

---
+++ Assets/Scripts/SoundDetectorTrap.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundDetectorTrap : TrapBase
+{
+    [Header("Sound Settings")]
+    public AudioClip triggerSound;
+    [Range(0f, 1f)] public float volume = 1f;
+    public bool playOnce = false;
+
+    private AudioSource audioSource;
+    private bool permanentlyDisabled = false;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+    }
+
+    protected override void ActivateTrap(PlayerController player)
+    {
+        if (permanentlyDisabled)
+            return;
+
+        if (triggerSound != null) {
+            audioSource.PlayOneShot(triggerSound, volume);
+        }
+
+        if (playOnce)
+            permanentlyDisabled = true;
+    }
+}
